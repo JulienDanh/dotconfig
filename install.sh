063ce43d34fdb2af92139b45216577d3dc62c252
Julien Danh <juliendanh@Juliens-Air-001.lan>
2023-12-31 16:23:15 +0100

    make brew installation only if not installed

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/juliendanh/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if which brew >/dev/null 2>&1; then
+    echo "Homebrew already installed"
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/juliendanh/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 brew bundle install
