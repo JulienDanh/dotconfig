356da2cf7da123a50cb40dfc02507cc04d659ce0
Julien Danh <julien@Juliens-Air-001.lan>
2024-01-26 20:58:52 +0100

    add echo path to zshrc

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 if which brew >/dev/null 2>&1; then
-    echo "Homebrew already installed"
+	echo "Homebrew already installed"
 else
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/juliendanh/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	(
+		echo
+		echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+	) >>/Users/juliendanh/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+	export PATH="/opt/homebrew/bin:$PATH" >>~/.zshrc
 fi
 brew bundle install
 
